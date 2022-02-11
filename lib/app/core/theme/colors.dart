4bfee60596c9aacdc1e0487f8dd694a615dbf69d
Rafael Rodrigues Pedrosa <20181002800804@pucgo.edu.br>
2022-02-11 14:09:13 -0300

    Adicionando inputs na tela de login

--- lib/app/core/theme/colors.dart
+++ lib/app/core/theme/colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color get widgetBackgroundColor => const Color(0xff2C3333);
+  static Color get widgetBackgroundColor => const Color(0xff1B0F3D);
   static Color get primaryColor =>  const Color(0xff395b64);
-  static Color get primaryBackgroundColor => const Color(0xff1a1a1a);
+  static Color get primaryBackgroundColor => const Color(0xff1B0F3D);
+  static Color get secondBackgroundColor => const Color(0xff352E5A);
   static Color get primaryIconColor => const Color(0xff2666CF);
   static Color get primaryTextColor =>  Colors.white;
 }
--- lib/app/modules/auth/auth_login_page.dart
+++ lib/app/modules/auth/auth_login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:mycrypto/app/core/theme/colors.dart';
 
 class AuthLoginPage extends StatefulWidget {
@@ -11,11 +12,117 @@ class AuthLoginPage extends StatefulWidget {
 class _AuthLoginPageState extends State<AuthLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.primaryBackgroundColor,
-      child: Column(
-        children: <Widget> [
-        ],
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Container(
+          color: AppColors.primaryIconColor,
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+              Container(
+                padding: const EdgeInsets.all(30.0),
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.primaryColor.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 20,
+                      offset: const Offset(4, 7),
+                      // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 30.0,
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Usuário',
+                          labelStyle: TextStyle(
+                            color: AppColors.primaryIconColor.withOpacity(0.5),
+                          ),
+                          hintText: 'Usuário',
+                          hintStyle: TextStyle(
+                            color: AppColors.primaryIconColor.withOpacity(0.5),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: AppColors.primaryIconColor,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryIconColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryIconColor,
+                              width: 1.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(
+                            color: AppColors.primaryIconColor.withOpacity(0.5),
+                          ),
+                          hintText: 'Senha',
+                          hintStyle: TextStyle(
+                            color: AppColors.primaryIconColor.withOpacity(0.5),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: AppColors.primaryIconColor,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryIconColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            borderSide: BorderSide(
+                              color: AppColors.primaryIconColor,
+                              width: 1.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
