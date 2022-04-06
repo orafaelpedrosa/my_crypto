6a55485c9ddd843d8c1cbd272261c01ffe999412
rafaelrpedrosa <orafaelpedrosa@outlook.com>
2022-04-06 19:56:40 -0300

    Ajustando module auth

---
+++ lib/app/modules/auth/modules/login/login_module.dart
@@ -0,0 +1,12 @@
+import 'package:mycrypto/app/modules/auth/modules/login/login_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class LoginModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => LoginStore()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [];
+}
