2fdf6342b717c3f71ab81473b145c3794783c7ab
rafaelrpedrosa <orafaelpedrosa@outlook.com>
2022-04-07 00:08:59 -0300

    Ajustes Auth Android

--- lib/app/modules/auth/auth_repository.dart
+++ lib/app/modules/auth/auth_repository.dart
@@ -1,33 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthRepository with Disposable {
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   User? user;
-
-  Future<void> sigInWithGoogle() async {
-    final googleSignIn = GoogleSignIn();
-    final googleUser = await googleSignIn.signIn();
-    if (googleUser != null) {
-      final googleAuth = await googleUser.authentication;
-      if (googleAuth.idToken != null) {
-        final userCredential = await _firebaseAuth.signInWithCredential(
-          GoogleAuthProvider.credential(
-            idToken: googleAuth.idToken,
-            accessToken: googleAuth.accessToken,
-          ),
-        );
-        //return userCredential.user;
-      }
-    }else{
-      throw FirebaseAuthException(
-        message: "Sign in with google failed",
-        code: 'ERROR_ABORTED_BY_USER',
-      );
-    }
-  }
-
+  
   void authCheck() {
     _firebaseAuth.authStateChanges().listen(
       (User? user) {
